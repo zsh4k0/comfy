6c81e12875fb54163eb93a0517b637eca9fbc108
zsh4k0 <147769442+zsh4k0@users.noreply.github.com>
2026-01-30 04:51:20 -0600

    Fix directory change command in swarmui.sh

--- scripts/swarmui.sh
+++ scripts/swarmui.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /scripts/
 git clone https://github.com/mcmonkeyprojects/SwarmUI.git swarmui
-cd /swarmui/
+cd swarmui/
 ./launch-linux.sh --launch_mode none --host 0.0.0.0 --port 8488
